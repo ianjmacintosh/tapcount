3d93698c2766ff703f6c0e54653b1960f84a7de6
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-01-22 13:04:09 -0300

    docs: ✅ Assert app expectations

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,14 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/*
+App expectations:
+* Shows a counter
+* Counter starts at zero
+* Shows a timer
+* Shows a control area
+* The control area has a reset button
+* Tapping anywhere but the control area increments the counter
+* Tapping the reset button resets the counter
+*/
