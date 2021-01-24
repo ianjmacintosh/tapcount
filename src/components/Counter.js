3ae816c9d16270057693452a0471aad4023344a1
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-01-24 17:06:30 -0300

    Add basic styles

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,8 +11,7 @@ class Counter extends React.Component {
     }
 
     render() {
-        return (<div data-testid="counter-component">
-            <label htmlFor="count">Count: </label>
+        return (<div className="counter" data-testid="counter-component">
             <span id="count" data-testid="count" className={this.props.settingMax ? "editable" : ""}>{this.props.count}</span>
         </div>);
     }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="root">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
