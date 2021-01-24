151b94e069a83ba20eb6342a42f15bea9cb9ea21
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-01-24 17:06:48 -0300

    Fix weird milliseconds bug

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -50,12 +50,11 @@ class Timer extends React.Component {
 
     render() {
         return (<div data-testid="timer-component">
-            <label htmlFor="timer">Timer: </label>
             <time id="time" data-testid="time" data-elapsedtime={this.props.elapsedTime} className={this.props.settingMax ? "editable" : ""}>
                 <span data-testid="hours">{this.state.hours < 10 ? "0" + this.state.hours : this.state.hours}</span>:
                 <span data-testid="minutes">{this.state.minutes < 10 ? "0" + this.state.minutes : this.state.minutes}</span>:
                 <span data-testid="seconds">{this.state.seconds < 10 ? "0" + this.state.seconds : this.state.seconds}</span>.
-                <span data-testid="milliseconds">{this.state.milliseconds}</span>
+                <span data-testid="milliseconds">{this.state.milliseconds === 10 ? "0" : this.state.milliseconds}</span>
             </time>
         </div>);
     }
