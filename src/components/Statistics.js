9200bf4641a863a55a704fee197d34fea33759ad
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-01-27 17:08:30 -0300

    Expose getTimeObject function

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { getTimeObject } from "../utilities/utilities"
+
 import './Statistics.css';
 
 class Statistics extends React.Component {
