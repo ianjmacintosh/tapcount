9a0dfc289ec40617493d38b792986629110b1473
Ian MacIntosh <ianjmacintosh@users.noreply.github.com>
2021-01-27 10:57:50 -0600

    style: 💄 Style buttons (#22)
    * Style buttons much more nicely
    
    * Depress pause button when paused

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -49,7 +49,7 @@ class Controls extends React.Component {
                     <button onClick={this.handleMaxClick} data-testid="max-button">Set Max</button>
                 </li> */}
                 <li>
-                    <button onClick={this.handlePauseButtonClick} data-testid="pause-button">Start/Stop</button>
+                    <button className="pause-button" onClick={this.handlePauseButtonClick} data-testid="pause-button">Start/Stop</button>
                 </li>
                 <li>
                     <button onClick={this.handleResetBothClick} data-testid="reset-button">Reset</button>
